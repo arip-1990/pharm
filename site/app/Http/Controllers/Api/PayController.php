4214be0a805e1c88f2f3de7c25264ab2d091093c
Arip <dev@arip.info>
2022-04-28 10:02:14 +0300

    fix pay api

--- site/app/Http/Controllers/Api/PayController.php
+++ site/app/Http/Controllers/Api/PayController.php
@@ -15,25 +15,17 @@ class PayController
 {
     public function handle(Request $request): Response
     {
-        $data = [
-            'amount;' . $request->get('amount'),
-            'mdOrder;' . $request->get('mdOrder'),
-            'operation;' . $request->get('operation'),
-            'status;' . $request->get('status')
-        ];
-        $data = implode(';', $data) . ';';
+        Redis::publish('bot:pay', json_encode($request->all()));
+
+        $data = 'amount;' . $request->get('amount') . ';mdOrder;' . $request->get('mdOrder')
+            . ';operation;' . $request->get('operation') . ';status;' . $request->get('status') . ';';
 
         $publicKey = Storage::get('callback.cer');
 
         $binarySignature = hex2bin(strtolower($request->get('checksum')));
         $isVerify = openssl_verify($data, $binarySignature, $publicKey, OPENSSL_ALGO_SHA512);
 
-        $sendData = [
-            'key' => $publicKey,
-            'verified' => (string)$isVerify,
-            'request' => implode('|', $request->all()),
-        ];
-        Redis::publish('bot:pay', json_encode($sendData));
+        Redis::publish('bot:pay', 'verified => ' . $isVerify);
 
         if ($isVerify !== 1) {
             return new Response(status: SymfonyResponse::HTTP_PRECONDITION_FAILED);
@@ -49,24 +41,29 @@ class PayController
 
     private function checkStatus(Order $order, string $operation, int $status): void
     {
-        switch ($operation) {
-            case 'deposited':
-                if ($status === 1) {
-                    $order->changeStatusState(Status::STATE_SUCCESS);
-                    $order->sent();
-                }
-                elseif ($status === 0) {
+        if ($order->status === Status::STATUS_PAID) {
+            switch ($operation) {
+                case 'deposited':
+                    if ($status === 1) {
+                        $order->changeStatusState(Status::STATE_SUCCESS);
+                        $order->sent();
+                    }
+                    elseif ($status === 0) {
+                        $order->changeStatusState(Status::STATE_ERROR);
+                    }
+                    break;
+                case 'reversed':
                     $order->changeStatusState(Status::STATE_ERROR);
-                }
-                break;
-            case 'reversed':
-                $order->changeStatusState(Status::STATE_ERROR);
-                Exception::create($order->id, 'pay', 'Оплата отменена')->save();
-                break;
-            case 'declinedByTimeout':
-                $order->changeStatusState(Status::STATE_ERROR);
-                Exception::create($order->id, 'pay', 'Истек время ожидания оплаты')->save();
-                break;
+                    Exception::create($order->id, 'pay', 'Оплата отменена')->save();
+                    break;
+                case 'declinedByTimeout':
+                    $order->changeStatusState(Status::STATE_ERROR);
+                    Exception::create($order->id, 'pay', 'Истек время ожидания оплаты')->save();
+                    break;
+            }
+        }
+        else {
+            Redis::publish('bot:pay', 'Данный заказ не ожидает оплаты!');
         }
     }
 }
