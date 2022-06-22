83e7ef37bd64a81506a22a61e3d86680f0ea3784
Arip <dev@arip.info>
2022-06-22 10:50:55 +0300

    test

--- site/app/Console/Commands/TestCommand.php
+++ site/app/Console/Commands/TestCommand.php
@@ -8,6 +8,9 @@ use App\Models\Product;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 ini_set('memory_limit', -1);
 
@@ -18,13 +21,35 @@ class TestCommand extends Command
 
     public function handle(): int
     {
-//        $productIds = Offer::query()->select('product_id')->groupBy('product_id')->get()->pluck('product_id');
-//        /** @var Product $product */
-//        foreach (Product::query()->findMany($productIds) as $product) {
-//            $product->photos()->update(['status' => Photo::STATUS_CHECKED]);
-//        }
-        echo PHP_EOL;
-        $this->info(PHP_EOL . 'Очистка успешно завершена!');
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('A1', 'Код');
+        $sheet->setCellValue('B1', 'Штрих-код');
+        $sheet->setCellValue('C1', 'Наименование');
+        $sheet->getStyle('A1')->applyFromArray([
+            'font' => ['bold' => true],
+            'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER]
+        ]);
+        $sheet->getStyle('B1')->applyFromArray([
+            'font' => ['bold' => true],
+            'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER]
+        ]);
+        $sheet->getStyle('C1')->applyFromArray([
+            'font' => ['bold' => true],
+            'alignment' => ['horizontal' => Alignment::HORIZONTAL_CENTER]
+        ]);
+
+        /** @var Product $product */
+        foreach (Product::active()->doesntHave('photos')->get() as $i => $product) {
+            $sheet->setCellValue('A' . ($i + 2), $product->code);
+            $sheet->setCellValue('B' . ($i + 2), $product->barcode ?? '');
+            $sheet->setCellValue('C' . ($i + 2), $product->name);
+        }
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save('Товары без фото.xlsx');
+
+        $this->info(PHP_EOL . 'Процесс завершена!');
         return 0;
     }
 }
