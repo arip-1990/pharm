1f8262c731bb6c13c1617a2e12102a63fcf29b7a
Gadjimurad <abakarov_gm1@mail.ru>
2025-02-13 14:22:27 +0300

    add discount

--- client/components/card/index.tsx
+++ client/components/card/index.tsx
@@ -10,6 +10,13 @@ import { ICart } from "../../models/ICart";
 import Recipe from "./Recipe";
 import Favorite from "./Favorite";
 import Price from "./Price";
+import neww50 from "../../assets/images/neww50.png"
+import neww30 from "../../assets/images/neww30.png"
+import neww15 from "../../assets/images/neww30.png"
+import neww300 from "../../assets/images/neww300.jpg"
+import neww700 from "../../assets/images/neww700.png"
+import neww120 from "../../assets/images/neww120.png"
+import neww250 from "../../assets/images/neww250.png"
 
 import styles from "./Card.module.scss";
 
@@ -34,6 +41,24 @@ const Card: FC<Props> = ({ product }) => {
     }
   };
 
+  const discountModel = (disc: number):any => {
+    if ( disc == 50) {
+      return <Image src={neww50}/>
+    }else if (disc == 30){
+      return <Image src={neww30}/>
+    }else if (disc == 15){
+      return <Image src={neww15}/>
+    } else if (disc == 300){
+      return <Image src={neww300}/>
+    }else if (disc == 700){
+      return <Image src={neww700}/>
+    } else if (disc == 120){
+    return <Image src={neww120}/>
+    }else if (disc == 250){
+      return <Image src={neww250}/>
+    }
+  }
+
   return (
     <div
       className={styles.card}
@@ -42,7 +67,7 @@ const Card: FC<Props> = ({ product }) => {
       itemType="https://schema.org/Product"
     >
       <div className={styles.card_image}>
-        <Favorite product={product} />
+        {/*<Favorite product={product} />*/}
 
         <Image
           itemProp="image"
@@ -51,17 +76,19 @@ const Card: FC<Props> = ({ product }) => {
           src={product.photos[0]?.url || defaultImage}
           alt={product.name}
         />
+        <Recipe isRecipe={product.recipe}/>
+        {product.discount ? "" : <Favorite product={product} />}
 
-        <Recipe isRecipe={product.recipe} />
 
-        {product.discount && (
-          <div
-            className={classNames(styles.card_discount, {
-              [styles.card_discount__50]: Number(product.discount) === 50,
-              [styles.card_discount__30]: Number(product.discount) === 30,
-            })}
-          />
-        )}
+        {product.discount &&
+          discountModel(Number(product.discount))
+          // <div
+          //   className={classNames(styles.card_discount, {
+          //     [styles.card_discount__50]: Number(product.discount) === 50,
+          //     [styles.card_discount__30]: Number(product.discount) === 30,
+          //   })}
+          // />
+        }
       </div>
 
       <div className={styles.card_body}>
